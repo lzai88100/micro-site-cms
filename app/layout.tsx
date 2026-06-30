2115d31f374d08b3d81ea13773902f45541d6d5b
Lzai <zillling1810@gmail.com>
2026-07-01 02:56:11 +0800

    Update site title and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "藏精閣微官網",
-  description: "手機優先版微官網",
+  title: "藏精閣｜資訊中心",
+  description: "藏精閣｜微官網",
 };
 
 export default function RootLayout({
